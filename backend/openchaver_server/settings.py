819b60a5b02357d97698e298ef700155634118c0
dickermoshe <dickermoshe@gmail.com>
2022-10-06 19:11:36 -0400

    add logging

--- backend/openchaver_server/settings.py
+++ backend/openchaver_server/settings.py
@@ -175,3 +175,34 @@ CSRF_TRUSTED_ORIGINS = [
 
 
 
+
+# Setup logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    }
+}
+
