c3d4e09deb2c37be5327fa83ddf2a3382e45b77b
Nosson M Frankel <nossonmfrankel@gmail.com>
2022-10-06 19:57:21 +0300

    Initial Svelte render.com build

---
+++ frontend/svelte.config.ts
@@ -0,0 +1,18 @@
+import adapter from '@sveltejs/adapter-static'
+import preprocess from 'svelte-preprocess'
+import type { Config } from '@sveltejs/kit'
+
+const config: Config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
+	kit: {
+		adapter: adapter(),
+		prerender: {
+			default: true
+		}
+	}
+};
+
+export default config;
