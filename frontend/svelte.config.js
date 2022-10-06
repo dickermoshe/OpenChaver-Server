c1e6205e6f375ba3ac8f24358cf2ccdb91bc27b5
Nosson M Frankel <nossonmfrankel@gmail.com>
2022-10-06 21:30:54 +0300

    static-build is working

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,14 +1,16 @@
 import adapter from '@sveltejs/adapter-static'
 import preprocess from 'svelte-preprocess'
-import type { Config } from '@sveltejs/kit'
 
-const config: Config = {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			fallback: '200.html'
+		})
 	}
 };
 
