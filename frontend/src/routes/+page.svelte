b75eba24c011421ff07d67b0b8ebfa68585d7501
Nosson M Frankel <nossonmfrankel@gmail.com>
2022-10-06 22:31:04 +0300

    Finally working

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Please visit us again later.</h1>
+<p>We currently are working on our systems.</p>
