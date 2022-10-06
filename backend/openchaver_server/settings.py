db1175c29e648d8916cbfe91fa1c3404bb0be5e5
dickermoshe <dickermoshe@gmail.com>
2022-10-06 19:22:18 -0400

    update djaoser settings

--- backend/openchaver_server/settings.py
+++ backend/openchaver_server/settings.py
@@ -206,3 +206,11 @@ LOGGING = {
     }
 }
 
+DJOSER = {
+    'PASSWORD_RESET_CONFIRM_URL': '#/password/reset/confirm/{uid}/{token}',
+    'USERNAME_RESET_CONFIRM_URL': '#/username/reset/confirm/{uid}/{token}',
+    'ACTIVATION_URL': '#/activate/{uid}/{token}',
+    'SEND_ACTIVATION_EMAIL': True,
+    'SERIALIZERS': {},
+}
+
