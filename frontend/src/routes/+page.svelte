05b361aa6c77b5e180b2c6d85a6086f1528b49b4
Nosson M Frankel <nossonmfrankel@gmail.com>
2022-10-07 12:51:02 +0300

    Added meta tags

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<svelte:head>
+	<title>Home | OpenChaver</title>
+	<meta name="description" content="An open source alternative to WebChaver. In early stages of development." />
+</svelte:head>
+
 <h1>Please visit us again later.</h1>
 <p>We currently are working on our systems.</p>
