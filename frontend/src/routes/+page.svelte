b64b4307570b84d66b802523643d85b77a5546e9
Nosson M Frankel <nossonmfrankel@gmail.com>
2022-10-07 15:01:17 +0300

    Updated homepage

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -6,5 +6,7 @@
 	/>
 </svelte:head>
 
-<h1>Please visit us again later.</h1>
-<p>We currently are working on our systems.</p>
+<h1>Please visit us again later</h1>
+<p>Our site is not operational at the current time.</p>
+
+<p>View our progress or offer your help: <a href="https://github.com/dickermoshe/openchaver">Github repo</a></p>
