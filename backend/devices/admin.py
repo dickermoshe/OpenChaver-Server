ba3dff8f14df497f7af9391b1bad30a4d9dae706
dickermoshe <dickermoshe@gmail.com>
2022-10-07 18:17:14 -0400

    make migrations

--- backend/devices/admin.py
+++ backend/devices/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Device, Screenshot, Chaver
+
+admin.site.register(Device)
+admin.site.register(Screenshot)
+admin.site.register(Chaver)
+
--- backend/devices/models.py
+++ backend/devices/models.py
@@ -9,10 +9,20 @@ def get_random_code():
 # Create your models here.
 class Device(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='devices')
+    uuid = models.CharField(max_length=8, default=get_random_code, unique=True,)
     uninstall_code = models.CharField(max_length=8, default=get_random_code)
     name = models.CharField(max_length=100)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    @property
+    def install_uri(self):
+        return f'chaver://install/#uuid={self.uuid}&uninstall_code={self.uninstall_code}'
+    
+    @property
+    def uninstall_uri(self):
+        return f'chaver://uninstall/#uuid={self.uuid}&uninstall_code={self.uninstall_code}'
+
     def __str__(self):
         return self.name
 
