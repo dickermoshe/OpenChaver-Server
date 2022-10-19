45806fbb0c729612fba6412cd892c10c09e1c370
Nosson M Frankel <nossonmfrankel@gmail.com>
2022-10-19 11:57:49 -0400

    Force logout on (/signup || /login)

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
+	import { page } from '$app/stores'
+	import { authToken } from '$lib/authToken'
 	import Header from '$lib/Header.svelte'
 	import '../app.css'
+
+	$: if($page.routeId === 'signup' || $page.routeId === 'login') authToken.set('')
 </script>
 
 <Header />
