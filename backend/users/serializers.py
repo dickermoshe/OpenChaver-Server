2b516156ec57ad682565fb898ea8e8a0f2f2d98c
dickermoshe <dickermoshe@gmail.com>
2022-10-20 14:19:41 -0400

    add devices

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -2,12 +2,15 @@ from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
 from djoser.serializers import UserSerializer as BaseUserSerializer
 from django.contrib.auth import get_user_model
 
+from devices.serializers import DeviceSerializer
+
 user_model = get_user_model()
 
 class UserSerializer(BaseUserSerializer):
+    devices = DeviceSerializer(many=True, read_only=True)
     class Meta(BaseUserSerializer.Meta):
         model = user_model
-        fields = ('id', 'email', 'is_active','first_name','last_name')
+        fields = ('id', 'email', 'is_active','first_name','last_name','devices')
 
 class UserCreateSerializer(BaseUserCreateSerializer):
     class Meta(BaseUserCreateSerializer.Meta):
