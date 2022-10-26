ee2f7f7c11acacfd5e4c787ee4e5b0d387564f0f
dickermoshe <dickermoshe@gmail.com>
2022-10-26 14:01:36 -0400

    update admin

--- backend/devices/admin.py
+++ backend/devices/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-
+from django.utils.html import format_html
 # Register your models here.
 from .models import Device, Screenshot, Chaver, Log
 
@@ -12,6 +12,16 @@ class DeviceAdmin(admin.ModelAdmin):
     actions = [send_reports]
 
 admin.site.register(Device, DeviceAdmin)
-admin.site.register(Screenshot)
+
+class ScreenshotAdmin(admin.ModelAdmin):
+    list_display = ['title','device_html', 'nsfw', 'profane','created']
+    list_filter = ['nsfw', 'profane']
+    search_fields = ['title', 'device__name']
+
+    def device_html(self, obj):
+        return format_html('<a href="/admin/devices/device/{}/change/">{}</a>', obj.device.id, obj.device.name)
+
+admin.site.register(Screenshot, ScreenshotAdmin)
+
 admin.site.register(Chaver)
 admin.site.register(Log)
